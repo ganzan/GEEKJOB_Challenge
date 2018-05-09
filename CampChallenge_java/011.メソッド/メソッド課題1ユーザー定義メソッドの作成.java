84e6a2915ecb5d0ba78ddf61159a2de421ef3d60
DESKTOP-APGUQKI\guest1Day <ganzan297@gmail.com>
2018-05-09 15:18:16 +0900

    メソッド課題1

---
+++ CampChallenge_java/011.メソッド/メソッド課題1ユーザー定義メソッドの作成.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class メソッド課題1ユーザー定義メソッドの作成 extends HttpServlet {
+    
+    
+    void myprofile(PrintWriter pw) {
+        pw.print("丸山　瑛久<br>");
+        pw.print("1995年9月29日<br>");
+        pw.print("趣味はゲームで好きな食べ物はラーメンです<br><br>");
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet メソッド課題1ユーザー定義メソッドの作成</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet メソッド課題1ユーザー定義メソッドの作成 at " + request.getContextPath() + "</h1>");
+            myprofile(out);
+            myprofile(out);
+            myprofile(out);
+            myprofile(out);
+            myprofile(out);
+            myprofile(out);
+            myprofile(out);
+            myprofile(out);
+            myprofile(out);
+            myprofile(out);
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
