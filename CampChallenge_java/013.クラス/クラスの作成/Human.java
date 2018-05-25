646e8ed8cebc1760618fd7a5dc2e1d7374827b32
DESKTOP-APGUQKI\guest1Day <ganzan297@gmail.com>
2018-05-25 18:08:01 +0900

    クラスの作成課題

---
+++ CampChallenge_java/013.クラス/クラスの作成/Human.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Human {
+    
+    public String name = "";
+    public int age = 0;
+    
+    public void setHuman(String n,int a){
+        
+        this.name = n;
+        this.age = a;
+    }
+    
+    
+}
---
+++ CampChallenge_java/013.クラス/クラスの作成/Test.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Test {
+    
+    public static void main(String[]args){
+        
+        Human hito = new Human();
+        
+        
+        hito.setHuman("名前",15);
+        
+        System.out.println(hito.name);
+        System.out.println(hito.age);
+    }
+}
+    
+        
+        
